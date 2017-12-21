30b1fa4777d00bca9e15d10dad0f8e9f6acb33b5
Celina Sophie Kalus <hello@celinakalus.de>
2017-12-21 23:22:00 +0100

    Updated second stage Added simple print and clear methods for second stage

--- second_stage/main.c
+++ second_stage/main.c
@@ -1,13 +1,26 @@
 #include "linker.h"
 
-void copy_kernel(void) {
-    uint8_t *display = (uint8_t*)0xB8000;
-    uint8_t *text = (uint8_t*)"THIS WORKS";
-    
-    while (*text) {
-        *display++ = *text++;
-        *display++ = 0x0F;
+uint8_t *display = (uint8_t*)0xB8000;
+
+void simple_clear(void) {
+    display = (uint8_t*)0xB8000;
+
+    for (int i = 0; i < 80 * 25; i++) {
+        display[2 * i] = 0x00;
+        display[2 * i + 1] = 0x00;
     }
+}
+
+void simple_print(const char* src) {
+    while (*src) {
+        *display++ = *src++;
+        *display++ = 0x02;
+    }
+}
+
+void copy_kernel(void) {
+    simple_clear();
+    simple_print("COPYING_KERNEL...");
 
     uint8_t *kernel_dest = (uint8_t*)0x00100000;
     uint8_t *kernel_src = (SS_VMA + (KERNEL_LMA - SS_LMA));
@@ -15,4 +28,6 @@ void copy_kernel(void) {
     for (uint32_t i = 0; i < KERNEL_SIZE; i++) {
         kernel_dest[i] = kernel_src[i];
     }
+
+    simple_print("DONE.");
 }
