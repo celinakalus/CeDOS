1adaeff5dda395594de7dcecee660306fa04da7d
Celina Sophie Kalus <hello@celinakalus.de>
2023-12-06 00:45:03 +0100

    ticks: Interval mode
    ticks user program now takes an optional argument, an integer
    representing an interval in ticks. The program will print a string each
    time the interval passes.

--- shell/ticks.c
+++ shell/ticks.c
@@ -2,8 +2,36 @@
 #include "stdio.h"
 
 #include <stdint.h>
+#include "stdlib.h"
+#include "string.h"
+
+#include "cedos.h"
 
 void main(char *args) {
-    int ticks = sc_time_get_ticks();
-    printf("%i\n", ticks);
+    if (strlen(args) == 0) {
+        int ticks = sc_time_get_ticks();
+        printf("%i\n", ticks);
+    } else {
+        int interval = atoi(args);
+
+        if (interval <= 0) {
+            printf("Invalid interval value.\n");
+            return;
+        }
+
+        int pid = get_pid();
+
+        int superticks = 0;
+
+        while (1) {
+            int ticks = sc_time_get_ticks();
+
+            if (ticks / interval != superticks) {
+                printf("[%i] %i ticks\n", pid, ticks);
+                superticks = ticks / interval;
+            }
+
+            yield();
+        }
+    }
 }
